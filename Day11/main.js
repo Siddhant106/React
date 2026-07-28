6cbf7b0785c58d1b18e3c9db5e4d8e56c4cdc14a
Siddhant Shukla <shuklasiddhant302@gmail.com>
2026-07-28 05:47:52 +0530

    My Tenth commit

---
+++ Day11/main.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+import Home from "./src/home";
+import Contact from "./src/contact";
+import Dashboard from "./src/dashboard";
+import Details from "./src/details";
+import Zero from "./src/zero";
+import Hello from "./src/hello";
+import Hi from "./src/hi";
+
+
+function App(){
+
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/details">Details</Link> 
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/contact" element={<Contact/>}></Route>
+                <Route path="/dashboard" element={<Dashboard/>}></Route>
+                <Route path="/details" element={<Details/>}>
+                    <Route index element={<Zero/>}></Route>
+                    <Route path="hello" element={<Hello/>}></Route>
+                    <Route path="hi" element={<Hi/>}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
