3159f7fc1095f124f841de2feced401115c4ffcd
Siddhant Shukla <shuklasiddhant302@gmail.com>
2026-08-07 04:37:19 +0530

    My Sixteenth commit

---
+++ Day15/src/app.js
@@ -0,0 +1,173 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    // <h1 className="bg-gray-400 text-xl rounded-xl p-2 m-2 border-2">Hello bhaiii</h1>
+    <div className="flex flex-wrap justify-center gap-10">
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-emerald-100 shadow-md m-5 rounded-xl">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkimnLygRzr4nMr9OD_EowonMxkcSc3K6N72XzUX1HlA&s=10" className="w-full h-48 object-cover p-3 rounded-2xl"></img>
+            </div>
+
+            <div className="mt-2 p-2 font-bold text-3xl text-gray-700">
+                <h1>Appreciation</h1>
+            </div>
+            
+            <div className="text-indigo-900 p-2 text-xl mt-2">
+                <p>You bring curiosity and dedication together in a way that makes learning feel inspiring—keep shining! 🌟</p>
+            </div>
+            
+            <div className="mt-3 p-2 flex justify-center">
+                <button className="bg-sky-500 p-1 rounded-xl text-fuchsia-50 hover:bg-sky-700">Know More</button>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
